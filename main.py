2274b825a65866d07fc4c15a139e8005dbf44085
Schlabaga <78259357+Schlabaga@users.noreply.github.com>
2023-12-31 11:04:12 +0100

    Mise en place de classes contenant les fonctions/ code + organisé

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from datetime import datetime
@@ -7,8 +8,7 @@ app = Flask(__name__)
 def creer_connexion():
     return sqlite3.connect('emploidutemps.db')
 
-def initialiser_base_de_donnees():
-    conn = creer_connexion()
+def initialiser_base_de_donnees(conn):
     with conn:
         conn.execute('''
             CREATE TABLE IF NOT EXISTS taches (
@@ -19,35 +19,73 @@ def initialiser_base_de_donnees():
             )
         ''')
 
-@app.route('/')
-def accueil():
-    conn = creer_connexion()
-    with conn:
-        cursor = conn.execute('SELECT * FROM taches ORDER BY date')
-        taches = cursor.fetchall()
-    return render_template('accueil.html', taches=taches)
+class Tache:
+    
+    def __init__(self, id, titre, contenu, date=None):
+        self.id = id
+        self.titre = titre
+        self.contenu = contenu
+        self.date = date or datetime.utcnow()
 
-@app.route('/ajouter_tache', methods=['POST'])
-def ajouter_tache():
-    if request.method == 'POST':
-        titre = request.form['titre']
-        contenu = request.form['contenu']
-        conn = creer_connexion()
-        with conn:
-            conn.execute('INSERT INTO taches (titre, contenu) VALUES (?, ?)', (titre, contenu))
-    return redirect(url_for('accueil'))
+    def __str__(self):
+        return f"Tâche {self.titre} ({self.date})"
 
 
-@app.route('/supprimer_tache/<int:id>')
-def supprimer_tache(id):
-    conn = creer_connexion()
-    with conn:
-        conn.execute('DELETE FROM taches WHERE id = ?', (id,))
-    return redirect(url_for('accueil'))
+class GestionnaireTaches:
+    
+    def __init__(self, base_de_donnees):
+        self.base_de_donnees = base_de_donnees
+
+    def ajouter_tache(self, titre, contenu):
+        with self.base_de_donnees as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO taches (titre, contenu) VALUES (?, ?)', (titre, contenu))
+
+    def supprimer_tache(self, tache_id):
+        with self.base_de_donnees as conn:
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM taches WHERE id = ?', (tache_id,))
+
+    def recuperer_taches(self):
+        with self.base_de_donnees as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT id, titre, contenu, date FROM taches ORDER BY date')
+            return [Tache(*row) for row in cursor.fetchall()]
 
 
+class ApplicationFlask:
+    
+    def __init__(self, base_de_donnees):
+        self.app = Flask(__name__)
+        self.gestionnaire_taches = GestionnaireTaches(base_de_donnees)
+        self.configurer_routes()
 
+    def configurer_routes(self):
+        self.app.route('/')(self.accueil)
+        self.app.route('/ajouter_tache', methods=['POST'])(self.ajouter_tache)
+        self.app.route('/supprimer_tache/<int:id>')(self.supprimer_tache)
+
+    def accueil(self):
+        taches = self.gestionnaire_taches.recuperer_taches()
+        return render_template('accueil.html', taches=taches)
+
+    def ajouter_tache(self):
+        if request.method == 'POST':
+            titre = request.form['titre']
+            contenu = request.form['contenu']
+            self.gestionnaire_taches.ajouter_tache(titre, contenu)
+        return redirect(url_for('accueil'))
+
+    def supprimer_tache(self, id):
+        self.gestionnaire_taches.supprimer_tache(id)
+        return redirect(url_for('accueil'))
+
+    def demarrer(self):
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-    initialiser_base_de_donnees()
-    app.run(debug=True)
+    base_de_donnees = sqlite3.connect('emploidutemps.db', check_same_thread=False)
+    initialiser_base_de_donnees(base_de_donnees)
+    
+    app_flask = ApplicationFlask(base_de_donnees)
+    app_flask.demarrer()
